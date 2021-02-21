cb7a9252673303a9dac7d93236c509dc8bd10a40
Akos Stegner <a.stegner@mito.hu>
2021-02-21 19:06:14 +0100

    Develop BaseInput

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="input">
+  <label v-if="label" class="mb-1">{{ label }}</label>
+  <div :class="mainClasses">
     <slot name="before"></slot>
-    <input
-      :value="modelValue"
-      v-bind="$attrs"
-      @input="$emit('update:modelValue', $event.target.value)"
-    />
+    <input :value="modelValue" v-bind="$attrs" @input="$emit('update:modelValue', $event.target.value)" />
     <slot name="after"></slot>
   </div>
+  <p v-if="error" class="mt-2 text-xs text-red-500 font-semibold uppercase">{{ error }}</p>
 </template>
 
 <script>
@@ -17,9 +15,30 @@ export default {
   props: {
     modelValue: {
       type: [Number, String],
-      default: ''
+      default: '',
+    },
+
+    label: {
+      type: String,
+      default: '',
+    },
+
+    error: {
+      type: String,
+      default: '',
+    },
+
+    size: {
+      type: String,
+      validator: value => ['xs', 'md'].includes(value),
+      default: 'md'
     },
   },
 
-}
+  computed: {
+    mainClasses() {
+      return ['input', [`is-${this.size}`], { 'is-error': this.error }];
+    },
+  },
+};
 </script>
